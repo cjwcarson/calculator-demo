7766b6cc83e650468e676f7c06a3db08320cca49
Chrisy Carson <cjwcarson@gmail.com>
2021-12-21 22:17:18 +0000

    Fixed a couple of typos Now dont need to init operators.

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -1,16 +1,13 @@
 <?php
 namespace App\Controller;
 
-use App\Service\Calculator;
+use App\Services\Calculator;
 use App\Entity\Calculation;
 use App\Form\Type\CalculatorType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class CalculatorController extends AbstractController
 {
